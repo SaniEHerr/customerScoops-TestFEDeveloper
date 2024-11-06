c48c75d620f1c0b51d0fd41a36d49c58e75c975f
SaniEHerr <santiagoeherrera@gmail.com>
2024-11-06 10:09:27 -0300

    fix(front): :art: Fix some console errors relates with svg properties

--- front/src/assets/icons/ArrowSmLeft.tsx
+++ front/src/assets/icons/ArrowSmLeft.tsx
@@ -2,7 +2,7 @@ const ArrowSmLeft = () => {
   return (
     <>
       <svg width="24px" height="24px" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-        <path d="M6 12H18M6 12L11 7M6 12L11 17" stroke="#000000" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+        <path d="M6 12H18M6 12L11 7M6 12L11 17" stroke="#000000" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
       </svg>
     </>
   )
